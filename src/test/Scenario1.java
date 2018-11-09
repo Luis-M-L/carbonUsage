a4198914c063e1f161d646e34e3dbe613ddb4798
Luis <luis@localhost.localdomain>
2018-11-09 14:32:53 +0100

    Printing unordered list of forecasts

--- src/test/Scenario1.java
+++ src/test/Scenario1.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.Test;
 
@@ -11,14 +15,17 @@ public class Scenario1 {
 
     @Test
     public void getCarbonIntensityEachRegion(){
-        Map<String, String> forecasts;
+        Map<Integer, Integer> forecasts = new HashMap<Integer, Integer>();
         Double sum = 0.0;
         for (int regId = 1; regId <= 17; regId++) {
             Response response = given().when().get(BASE_PATH + "/regional/regionid/" + regId).then()
                                 .statusCode(200).extract().response();
-            // String key = data[0].regions[0].regionid
-            // String value = data[0].regions[0].intensity.forecast
-            // forecasts.put(key, value);
+
+            JsonPath json = response.jsonPath();
+
+            int key = regId;
+            int value = extractForecast(json);
+            forecasts.put(key, value);
             // Collections.sort(forecasts, sorter)
             // Double carbon;
             // for(String generator: data[0].regions[0].generationmix){
@@ -26,7 +33,18 @@ public class Scenario1 {
             //          sum += generator.perc
             // ]
         }
+        System.out.println(forecasts.toString());
         // assertEquals(100 == sum)
+    }
+
+    public int extractForecast(JsonPath json){
+        ArrayList<String> data = json.get("data");
+        Object data0 = data.get(0);
+        Object regionData = ((HashMap<Object, Object>)data0).get("data");
+        Object regionData0 = ((List)regionData).get(0);
+        Object intensity = ((HashMap<Object, Object>)regionData0).get("intensity");
+        Object forecast = ((HashMap<Object, Object>)intensity).get("forecast");
+        return (Integer) forecast;
     }
 
     // class sorter
